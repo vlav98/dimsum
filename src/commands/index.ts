719a6dc086ea7effc239720334dd47460d9eae8c
Valentine Lav <valentine.lav@kooalys.com>
2022-12-11 01:36:48 +0100

    ✨ Added random prompt generators

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,5 +1,12 @@
 import { Command } from "src/interfaces/Command";
 import { Hello } from "./Hello.js";
 import { help } from "./help.js";
+import { randomPrompt } from "./randomPrompt.js";
+import { twoCharPrompt } from "./twoCharPrompt.js";
 
-export const CommandList: Command[] = [Hello, help];
+export const CommandList: Command[] = [
+  Hello,
+  help,
+  twoCharPrompt,
+  randomPrompt,
+];
